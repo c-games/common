c850cb9606d1d79a2bd2bc6daae34d1c7480c3ae
syber <syber.cg@gmail.com>
2019-05-23 17:31:27 +0800

    Add unit test of common/db

--- db/db_test.go
+++ db/db_test.go
@@ -1,13 +1,65 @@
 package db
 
 import (
+	"github.com/DATA-DOG/go-sqlmock"
+	"gitlab.3ag.xyz/core/backend/common/testutil"
 	"testing"
-	"github.com/golang/mock/gomock"
 )
 
+func TestDBAdapter_Exec(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	testutil.TestFailIfErr(t, err, "Can't not open sqlmock")
 
-func TestConnect(t *testing.T) {
-	ctrl := gomock.NewController(t)
+	adp := ConnectByDB(db)
 
-	defer ctrl.Finish()
+	mock.ExpectExec(`SELECT (.+) FROM orders WHERE 1`).
+		WillReturnResult(sqlmock.NewResult(0, 0))
+	mock.ExpectClose()
+
+	_ = adp.Exec("SELECT id FROM orders WHERE 1")
+
+	err = adp.Close()
+	testutil.TestFailIfErr(t, err, "Can't not close db connection")
+}
+
+func TestDBAdapter_QueryRow(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	testutil.TestFailIfErr(t, err, "Can't not open sqlmock")
+
+	adp := ConnectByDB(db)
+
+	mock.ExpectQuery("SELECT (.+) FROM orders WHERE game_id = (.+)").
+		WillReturnRows(sqlmock.NewRows([]string{"test", "123"}))
+
+	mock.ExpectClose()
+
+	rows := adp.QueryRow("SELECT id FROM orders WHERE game_id = ?", 1234)
+
+	// TODO How To check rows?
+	t.Log(rows)
+
+	err = adp.Close()
+	testutil.TestFailIfErr(t, err, "Can't not close db connection")
+}
+
+func TestDBAdapter_PrepareQuery(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	testutil.TestFailIfErr(t, err, "Can't not create sql mock")
+
+	adp := ConnectByDB(db)
+
+	mock.ExpectPrepare("SELECT (.+) FROM orders WHERE game_id = (.+)")
+
+	mock.ExpectQuery("SELECT (.+) FROM orders WHERE game_id = (.+)").
+		WillReturnRows(sqlmock.NewRows([]string{"test", "123"}))
+
+	mock.ExpectClose()
+
+	rows := adp.PrepareQuery("SELECT id FROM orders WHERE game_id = ?", 1234)
+
+	// TODO How To check rows?
+	t.Log(rows)
+
+	err = adp.Close()
+	testutil.TestFailIfErr(t, err, "Can't not close db connection")
 }
