57d6f7d8aa6ddec1c7b025e58c58cbb54f854351
syber <syber@gmail.com>
2019-06-22 18:12:33 +0800

    Fix error code const

---
+++ mq/msg/wallet/errcode.go
@@ -0,0 +1,10 @@
+package wallet
+
+const (
+	Code_Success         int = 0
+	Code_WalletNotFound  int = 1
+	Code_CreditNotEnough int = 2
+	Code_RegisterFailed  int = 3
+	Code_NoUpdateReason  int = 4
+)
+
---
+++ mq/msg/wallet/reason_code.go
@@ -0,0 +1,11 @@
+package wallet
+
+const (
+	Reason_Other       int = 0
+	Reason_Bid         int = 1 // 下注
+	Reason_Reward      int = 2 // 中獎
+	Reason_Withdraw    int = 3 // 撤獎
+	Reason_Rollback    int = 4
+	Reason_UpdateOrder int = 5
+)
+
