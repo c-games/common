1b7087b6057ca0988e13ef95c01e507fcffcef5f
syber <syber.cg@gmail.com>
2019-07-17 11:05:22 +0800

    Fix typo

--- mq/msg/userMsg/errcode.go
+++ mq/msg/userMsg/errcode.go
@@ -2,17 +2,17 @@ package userMsg
 
 // User Error Code Defined
 const (
-	Code_Success              int = 0
-	Code_TokenWrong           int = 1
-	Code_TokenExpired         int = 2
-	Code_AccountNotFound      int = 3
-	Code_WrongPassword        int = 4
-	Code_RequireFieldEmpty    int = 5
-	Code_LogoutFailed         int = 6
-	Code_UnknownAgentId       int = 7
-	Code_IdNotExist           int = 8
-	Code_AccountAllreadyExist int = 9
-	Code_ZeroDataUpdate       int = 10
-	Code_DbFailed             int = 11
-	Code_NotExistCommand      int = 12
+	Code_Success             int = 0
+	Code_TokenWrong          int = 1
+	Code_TokenExpired        int = 2
+	Code_AccountNotFound     int = 3
+	Code_WrongPassword       int = 4
+	Code_RequireFieldEmpty   int = 5
+	Code_LogoutFailed        int = 6
+	Code_UnknownAgentId      int = 7
+	Code_IdNotExist          int = 8
+	Code_AccountAlreadyExist int = 9
+	Code_ZeroDataUpdate      int = 10
+	Code_DbFailed            int = 11
+	Code_NotExistCommand     int = 12
 )
