5c8bcea3aecb85c71dae4b6072252c9cd0c9b61a
syber <syber.cg@gmail.com>
2019-09-20 01:26:59 +0800

    Add new wallet msg define

--- mq/msg/walletMsg/wallet.go
+++ mq/msg/walletMsg/wallet.go
@@ -1,9 +1,12 @@
 package walletMsg
 
-type UpdateData struct {
-//	MasterAgentId   int     `json:"master_agent_id"`
-//	AgentId         int     `json:"agent_id"`
-	UserId          int64   `json:"user_id"`
+type OrderChangeRecord struct {
+	OrderId int64
+	Change  float32
+	Reason  int
+}
+
+type OrderRecord struct {
 	CreditChange    float32 `json:"credit_change"`
 	Reason          int     `json:"reason"`
 	ReasonMessage   string  `json:"reason_message"`
@@ -13,9 +16,27 @@ type UpdateData struct {
 	AgentTransferId string  `json:"agent_transfer_id"`
 }
 
+type UpdateData struct {
+	UserId          int64               `json:"user_id"`
+	CreditChange    float32             `json:"credit_change"`
+	Reason          int                 `json:"reason"`
+	ReasonMessage   string              `json:"reason_message"` // 暫時留下來
+	Detail          []OrderChangeRecord `json:"detail"`
+	GameId          int                 `json:"game_id"`
+	OrderId         int64               `json:"order_id"`
+	TransferId      int64               `json:"transfer_id"`
+	AgentTransferId string              `json:"agent_transfer_id"`
+}
+
+type MultiUpdateData struct {
+	UserId      int64         `json:"user_id"`
+	TotalCredit float32       `json:"total_credit"`
+	Detail      []OrderRecord `json:"detail"`
+}
+
 type ValidateData struct {
-	UserId        int64   `json:"user_id"`
-	ExpectCredit  float32 `json:"expect_credit"`
+	UserId       int64   `json:"user_id"`
+	ExpectCredit float32 `json:"expect_credit"`
 }
 
 type RegisterData struct {
@@ -26,13 +47,10 @@ type RegisterData struct {
 }
 
 type QueryData struct {
-//	MasterAgentId int   `json:"master_agent_id"`
-//	AgentId       int   `json:"agent_id"`
-	UserId        int64 `json:"user_id"`
+	UserId int64 `json:"user_id"`
 }
 
 // response data
-
 type ResponseData struct {
 	Credit float32 `json:"credit"`
 }
