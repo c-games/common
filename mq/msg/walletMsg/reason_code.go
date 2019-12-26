43b69a67ce61b1f0f11f9eea9815b821d4558954
syber <syber.cg@gmail.com>
2019-12-26 22:23:58 +0800

    Add a new function to check wallet reasons

--- mq/msg/walletMsg/reason_code.go
+++ mq/msg/walletMsg/reason_code.go
@@ -12,6 +12,16 @@ Reason_MaxRange int = 9
 
 )
 
+func IsAvailableReason(reason int) bool {
+	switch reason {
+	case Reason_Bid, Reason_Reward, Reason_Withdraw, Reason_Transfer_In, Reason_Transfer_Out, Reason_Flat:
+		return true
+	default:
+		return false
+	}
+}
+
+
 func GetReasonString(reasonCode int) string {
 	switch reasonCode {
 	case Reason_Bid:
