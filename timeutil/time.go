a32e06431633c744b0da75e93a0cb19e52e07589
syber <syber.cg@gmail.com>
2020-01-11 02:01:13 +0800

    Add ParseCGTime, FormatTimeToDate

--- timeutil/time.go
+++ timeutil/time.go
@@ -6,6 +6,14 @@ func FormatCGTime(t time.Time) string {
 	return t.Format("2006-01-02 15:04:05")
 }
 
+func ParseCGTime(ts string) (time.Time, error) {
+	return time.Parse( "2006-01-02 15:04:05", ts)
+}
+
+func FormatTimeToDate(t time.Time) string {
+	return t.Format("2006-01-02")
+}
+
 func Now() string {
 	return FormatCGTime(time.Now())
 }
