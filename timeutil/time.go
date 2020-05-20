2a879d3f61370a03da78f3565d323f1a1ef9e5d0
syber <syber.cg@gmail.com>
2020-05-21 00:28:27 +0800

    Add MaybeFormatCGTime

--- timeutil/time.go
+++ timeutil/time.go
@@ -1,9 +1,20 @@
 package timeutil
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
 
 func FormatCGTime(t time.Time) string {
 	return t.Format("2006-01-02 15:04:05")
+}
+
+func MaybeFormatCGTime(t sql.NullTime) string {
+	if t.Valid {
+		return FormatCGTime(t.Time)
+	}
+	return ""
+
 }
 
 func ParseCGTime(ts string) (time.Time, error) {
