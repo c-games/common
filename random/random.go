bcc558fcd988957667853670b9636b6e94fc2ba5
jerryChang <jerryChang.cg@gmail.com>
2020-12-11 14:23:54 +0800

    add - add random number

--- random/random.go
+++ random/random.go
@@ -8,6 +8,7 @@ import(
 
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+var numberRunes = []rune("1234567890")
 
 func Init() {
 	rand.Seed(time.Now().UnixNano())
@@ -22,6 +23,19 @@ func RandStringRunes(n int) string {
 	return string(b)
 }
 
+func RandNumberRunes(n int) string {
+	Init()
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = numberRunes[rand.Intn(len(numberRunes))]
+	}
+	return string(b)
+}
+
 func GenString(offset int) string {
 	return RandStringRunes(offset)
+}
+
+func GenNumber(offset int) string {
+	return RandNumberRunes(offset)
 }
